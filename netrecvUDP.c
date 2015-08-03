85604b1c22f64ee2522f6c0034101a281b217afd
Dennis Binder <dennis.binder@gmx.de>
2015-08-03 23:03:31 +0200

    ok

---
+++ netrecvUDP.c
@@ -0,0 +1,92 @@
+/*
+    server.c - interaktiver Netzwerk-Server
+*/
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+void err_exit(char *message)
+ {
+  perror(message);
+  exit(1);
+ }
+
+int main(int argc, char *argv[])
+{
+	static char buffer[1000];
+	int sock_fd,  port, err, length;
+	socklen_t addr_size;
+	struct sockaddr_in my_addr, client_addr;
+	fd_set input_fdset;
+
+	if ((argc != 2) || (strcmp(argv[1], "-h") == 0))
+	{
+		fprintf(stderr, "Usage: server port\n");
+		return(1);
+	}
+
+	if (sscanf(argv[1], "%d", &port) != 1)
+	{
+		fprintf(stderr, "server: Bad port number.\n");
+		return(1);
+	}
+
+	/*--- socket() ---*/
+	sock_fd = socket(AF_INET,SOCK_DGRAM,0);
+	if (sock_fd == -1) err_exit("server: Can't create new socket");
+   
+	bzero(&my_addr,sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    my_addr.sin_port=htons(port);
+
+	printf("port = %d\n", port);
+
+
+	/*--- bind() ---*/
+	err = bind(sock_fd, (struct sockaddr *)&my_addr, sizeof(my_addr));
+	if (err == -1) err_exit("server: bind() failed");
+
+	printf("bindm returned = %d\n", err);
+
+	while (1)
+	{
+		addr_size = sizeof(struct sockaddr_in);
+	    length = recvfrom(sock_fd, buffer, 256, 0,(struct sockaddr *)&client_addr, &addr_size);
+		buffer[length]=0;
+		printf("received: %s\n");
+	}
+
+	close(sock_fd);
+	return(0);
+
+   //int sockfd,n;
+   //struct sockaddr_in servaddr,cliaddr;
+   //socklen_t len;
+   //char mesg[1000];
+
+   //sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+   //bzero(&servaddr,sizeof(servaddr));
+   //servaddr.sin_family = AF_INET;
+   //servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+   //servaddr.sin_port=htons(2345);
+   //bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+   //for (;;)
+   //{
+   //   len = sizeof(cliaddr);
+   //   n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+   //   printf("-------------------------------------------------------\n");
+   //   mesg[n] = 0;
+   //   printf("Received the following:\n");
+   //   printf("%s",mesg);
+   //   printf("-------------------------------------------------------\n");
+   //}
+ }
---
+++ netsendUDP.c
@@ -0,0 +1,167 @@
+/*
+    netsend.c - einfacher "txt sender"
+*/
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+ {
+	static char buffer[256];
+	char telegram[1000];
+	int sock_fd, err, length, port;
+	struct sockaddr_in server_addr;
+	fd_set input_fdset;
+	int argnumber;
+
+	if (argc < 4)
+	{
+		fprintf(stderr, "Usage: connect ip-addr port 'text'\n");
+		return(1);
+	}
+
+	if (sscanf(argv[2], "%d", &port) != 1)
+	{
+		fprintf(stderr, "connect: bad argument '%s'\n", argv[2]);
+		return(1);
+	}
+
+	// UDP Socket erstellen
+	sock_fd=socket(AF_INET,SOCK_DGRAM,0);
+	if (sock_fd == -1)
+	{
+		perror("connect: Can't create new socket");
+		return(1);
+	}
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	err = inet_aton(argv[1], &(server_addr.sin_addr));
+	if (err == 0)
+	{
+		fprintf(stderr, "connect: Bad IP-Address '%s'\n", argv[1]);
+		return(1);
+	}
+
+	argnumber = 3;
+	telegram[0]=0;
+	do
+	{
+		if (sscanf(argv[argnumber], "%s", buffer) == 1)
+		{
+			strcat(telegram, buffer);
+			argnumber++;
+			if (argnumber == argc)
+			{
+				//strcat(buffer, "\n");
+			}
+			else
+			{
+				strcat(telegram, " ");
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	while(argnumber < argc);
+
+	length = strlen(telegram);
+	printf("==> %s\n",telegram);
+	telegram[length] = 0;
+
+	if (sendto(sock_fd, telegram, length+1,0,(struct sockaddr*)&server_addr,sizeof(server_addr)) == -1)
+	{
+		printf("error \n");
+		perror("sendto: sendto() failed");
+		close(sock_fd);
+		return(1);
+	}
+
+	close(sock_fd);
+
+	return(0);
+   //int sockfd,n;
+   //struct sockaddr_in servaddr,cliaddr;
+   //char sendline[1000];
+   //char recvline[1000];
+
+   //if (argc != 2)
+   //{
+   //   printf("usage:  udpcli <IP address>\n");
+   //   exit(1);
+   //}
+
+   //sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+   //bzero(&servaddr,sizeof(servaddr));
+   //servaddr.sin_family = AF_INET;
+   //servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+   //servaddr.sin_port=htons(2345);
+
+   //while (fgets(sendline, 10000,stdin) != NULL)
+   //{
+   //   sendto(sockfd,sendline,strlen(sendline),0, (struct sockaddr *)&servaddr,sizeof(servaddr));
+   //   //n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+   //   //recvline[n]=0;
+   //   //fputs(recvline,stdout);
+   //}
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///* Sample UDP client */
+//
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <stdio.h>
+//
+//int main(int argc, char**argv)
+//{
+//   int sockfd,n;
+//   struct sockaddr_in servaddr,cliaddr;
+//   char sendline[1000];
+//   char recvline[1000];
+//
+//   if (argc != 2)
+//   {
+//      printf("usage:  udpcli <IP address>\n");
+//      exit(1);
+//   }
+//
+//   sockfd=socket(AF_INET,SOCK_DGRAM,0);
+//
+//   bzero(&servaddr,sizeof(servaddr));
+//   servaddr.sin_family = AF_INET;
+//   servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+//   servaddr.sin_port=htons(32000);
+//
+//   while (fgets(sendline, 10000,stdin) != NULL)
+//   {
+//      sendto(sockfd,sendline,strlen(sendline),0,
+//             (struct sockaddr *)&servaddr,sizeof(servaddr));
+//      n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+//      recvline[n]=0;
+//      fputs(recvline,stdout);
+//   }
+//}
